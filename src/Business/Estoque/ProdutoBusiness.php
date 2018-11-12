f654445e31cb5cb7874450c65deed506c9a2d0b2
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2018-11-12 17:11:24 -0200

    Terminando saveImages

--- src/Business/Estoque/ProdutoBusiness.php
+++ src/Business/Estoque/ProdutoBusiness.php
@@ -465,9 +465,30 @@ class ProdutoBusiness
             }
         }
 
+        $ftpProductImagesCacheFolder = getenv('OC_FTP_PRODUCT_IMAGE_CACHE_FOLDER');
+        $this->recursiveDeleteDirFTP($ftpConn, $ftpProductImagesCacheFolder . '/' . $fornecedorFolder . '/' . $produtoFolder);
+
         ftp_close($ftpConn);
 
 
+    }
+
+    private function recursiveDeleteDirFTP($ftpConn, $dir)
+    {
+        // here we attempt to delete the file/directory
+        if (!(@ftp_rmdir($ftpConn, $dir) || @ftp_delete($ftpConn, $dir))) {
+            // if the attempt to delete fails, get the file listing
+            $filelist = @ftp_nlist($ftpConn, $dir);
+
+            // loop through the file list and recursively delete the FILE in the list
+            foreach ($filelist as $ent) {
+                if (pathinfo($ent)['basename'] == '.' or pathinfo($ent)['basename'] == '..') continue;
+                $this->recursiveDeleteDirFTP($ftpConn, $ent);
+            }
+
+            // if the file list is empty, delete the DIRECTORY we passed
+            $this->recursiveDeleteDirFTP($ftpConn, $dir);
+        }
     }
 
 }
