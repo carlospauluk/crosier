e85564eb1ca48177e4f10a6724716742bd811018
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2018-11-13 23:03:14 -0200

    Um erro que não é um erro.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+
+    private $logger;
 
     /**
      *
@@ -16,5 +20,31 @@ class DefaultController extends Controller
     {
         return $this->render('index.html.twig');
 
+    }
+
+    /**
+     * @Route("/logAnError", name="logAnError")
+     */
+    public function logAnError() {
+        $this->getLogger()->error('Um erro que não é um erro.');
+        return new Response('Errou!');
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getLogger(): LoggerInterface
+    {
+        return $this->logger;
+    }
+
+    /**
+     * @required
+     * @param mixed $logger
+     */
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 }
